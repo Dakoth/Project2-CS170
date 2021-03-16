338ac3f99a7d0c0d65f25956d7fbd641b6c3ac8f
Alfredo Gonzalez <56003346+Dakoth@users.noreply.github.com>
2021-03-15 17:39:43 -0700

    Done with 90% (From slides)

--- main.cpp
+++ main.cpp
@@ -11,6 +11,9 @@
 #include <limits>   //Used for infinity 
 
 
+#include <iomanip>  //setprecision
+
+
 #include <time.h>
 
 using namespace std; 
@@ -21,13 +24,8 @@ int cols = 0;  //counts the cols of the file
 //fstreams can both read in and out data, unlike just ifstream and ofstreams 
 
 
-//Takes in the fstream, 
-//Or maybe, just takes in the vector instead of fstream
-//takes in a pointer to 2d vector instead of fstream
-//Pass in the row number from loop?                                                                            
-//is feature to add just an int?
-
-                                                                       
+//Takes in the fstream,                                          
+//is feature to add just an int?                                                                       
 double leave_one_out_cross_validation(vector<vector<double>> &d, vector<double> &current_set, int feature_to_add) { //temporarily for testing 
     double accuracy; 
     
@@ -37,9 +35,14 @@ double leave_one_out_cross_validation(vector<vector<double>> &d, vector<double> 
     double nearest_neighbor_label; 
     double distance, sum;   //sum used for eucidean distance 
 
+    double number_correctly_classified = 0;
+
     //initalize vector with cols - 1 with all 0 
     vector<double> object_to_classify(cols - 1, 0);
-    double label_object_to_classify;
+    double label_object_to_classify = 0;
+
+    //cout << "object to classifiy is size: " << object_to_classify.size() << endl;
+    //cin >> label_object_to_classify;            //TEST
 
     //rows 
     //start at 1 or 0?
@@ -49,11 +52,25 @@ double leave_one_out_cross_validation(vector<vector<double>> &d, vector<double> 
 
         //initalize object to classify 
         //might need to edit with this size
-        for (int j = 1; j < cols - 1; ++j) { 
+        //Needed to make sure it gets all the features
+        for (int j = 1; j <= cols - 1; j++) { 
             object_to_classify[j - 1] = d[i][j];        //makes this the value from the data 
         }
 
         label_object_to_classify = d[i][0];
+        //cout << "Label to classify is: " << label_object_to_classify << endl;
+
+
+        //Test printing objecto to classify 
+        /*
+        for (int l = 0; l < object_to_classify.size(); l++) 
+            cout << setprecision(9) << object_to_classify[l] << endl;
+
+        int TEST;
+        cin >> TEST;
+
+        */
+        setprecision(9);  //Don't know if this makes a difference
 
         //From here: https://en.cppreference.com/w/cpp/types/numeric_limits/infinity
         nearest_neighbor_distance = numeric_limits<double>::infinity();
@@ -67,103 +84,48 @@ double leave_one_out_cross_validation(vector<vector<double>> &d, vector<double> 
             if (k != i) {   //Don't compare to yourself
 
                 //Calculate Euclidean distance
+                //MIGHT NEED TO FIX 
                 sum = 0;
                 double diff = 0;
-                for(int z = 1; z < cols; ++z){
-                        //sum += pow(object_to_classify[z - 1] - d[k][z], 2);
-                        diff = object_to_classify[z - 1] - d[k][z];
-                        sum += diff*diff;
+                for(int z = 1; z <= cols - 1; z++){     //Should be cols - 1?
+                        sum += pow(object_to_classify[z - 1] - d[k][z], 2);
+                        //diff = object_to_classify[z - 1] - d[k][z];
+                        //sum += diff*diff;
                 }
                 //sqrt(sum);
-                distance = sqrt(sum);          //FIX ME
+                distance = sqrt(sum);          
 
                 if (distance < nearest_neighbor_distance) {
                     nearest_neighbor_distance = distance;
-                    nearest_neighbor_location = k;
+                    nearest_neighbor_location = k;                      ///IS THIS CORRECT?????
                     nearest_neighbor_label = d[nearest_neighbor_location][0];
                 }
             }
         }
         
-        cout << "Object " << i + 1 << " is class " << label_object_to_classify << endl;
-        cout << "Its nearest_neighbor is " << nearest_neighbor_location << " which is in class " << nearest_neighbor_label << endl;
+        if (label_object_to_classify == nearest_neighbor_label) {
+            //cout << "Correct!" << endl;
+            number_correctly_classified += 1;
+        }
+        
+        //cout << "Object " << i + 1 << " is class " << label_object_to_classify << endl;
+        //cout << "Its nearest_neighbor is " << nearest_neighbor_location + 1 << " which is in class " << nearest_neighbor_label << endl;
+                                                //IS THIS CORRECT??????
 
         //cout << "Looping over i, at the " << i + 1 << " location" << endl;
         //cout << "The " << i + 1 << "th object is in class: " << label_object_to_classify << endl;
     }
-    return rand(); 
+    //cout << "number_coreclty is : " << number_correctly_classified << endl;
+    //cout << "Number of rows is : " << rows << endl;
+    accuracy = number_correctly_classified / (rows);    //number of rows is the size of the data
+
+    cout << "accuracy is " << accuracy << endl;
+    return accuracy; 
 } 
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 void feature_search(vector<vector<double>> &Data) { //string& file) {//, string& file) {need to take in text file as input 
-    //ifstream myFile; 
-
-    /*
-    fstream myFile; 
-    myFile.open(file);
-
-    //Method for getting rows + cols of a text file 
-    string line, item; 
-    while (myFile.is_open()) {
-        //Gets the number of cols and Rows from the file 
-        //Adapted from here: http://www.cplusplus.com/forum/beginner/228327/
-        while( getline(myFile, line)) {
-            rows++;
-            if ( rows == 1 ) {                // First row only: determine the number of columns
-                stringstream ss( line );      // Set up up a stream from this line 
-                while ( ss >> item ) cols++;  // Each item delineated by spaces adds one to cols
-            }   
-        }
-        //
-
-        //cols--; //accounts for the extra column counted
-        //Clears the fstream + restarts it to the beginning
-        //From here: https://stackoverflow.com/questions/7681555/resetting-the-end-of-file-state-of-a-ifstream-object-in-c
-        myFile.clear();
-        myFile.seekg(0, ios::beg);
-        //
-
-    
-        //Make a 2D array from the string stream
-        //double fileArray[rows][cols];
-        //vector< vector<double>> Data(rows, vector<double>(cols, 0));  //inialize a 2d vector filled with all 0s 
-        const int tempRows = rows;
-        const int tempCols = cols; 
-
-        //Adapted here: https://stackoverflow.com/questions/36708370/reading-from-txt-file-into-two-dimensional-array-in-c
-        //Reads from the file + places data into an array
-        
-        //double Data[tempRows][tempCols];
-        //would vector be better? 
-        //vector<vector<double>> Data;
-
-        //n rows, m columns 
-        //initalize an n x m vector with 0s 
-        vector<vector<double>> Data( rows, vector<double> (cols, 0)); 
-
-
-        for(int i = 0; i < rows; i++)
-            for(int j = 0; j < cols; j++)
-                myFile >> Data[i][j];
-
-
-        //test print of the array 
-        
-        for(int i = 0; i < rows; i++) { 
-            //cout << endl;
-            for(int j = 0; j < cols ; j++) {
-                cout << Data[i][j] << " "; 
-                //if (j = rows - 1) { cout << endl;}
-            }   
-            cout << endl;
-        }
-        
-
-        myFile.close();
-    }
-    */
-
     //psuedo code part from slides
     vector<double> current_set_of_features; //(cols); //initalize an empty set the size of the features
     double accuracy = 0; 
@@ -248,6 +210,7 @@ int main() {
     //Method for getting rows + cols of a text file 
     string line, item; 
     while (myFile.is_open()) {
+
         //Gets the number of cols and Rows from the file 
         //Adapted from here: http://www.cplusplus.com/forum/beginner/228327/
         while( getline(myFile, line)) {
@@ -270,8 +233,8 @@ int main() {
         //Make a 2D array from the string stream
         //double fileArray[rows][cols];
         //vector< vector<double>> Data(rows, vector<double>(cols, 0));  //inialize a 2d vector filled with all 0s 
-        const int tempRows = rows;
-        const int tempCols = cols; 
+        //const int tempRows = rows;
+        //const int tempCols = cols; 
 
         //Adapted here: https://stackoverflow.com/questions/36708370/reading-from-txt-file-into-two-dimensional-array-in-c
         //Reads from the file + places data into an array
@@ -296,13 +259,15 @@ int main() {
         /*
         for(int i = 0; i < rows; i++) { 
             //cout << endl;
-            for(int j = 0; j < cols ; j++) {
+            for(int j = 1; j <= cols - 1; j++) {
                 cout << Data[i][j] << " "; 
                 //if (j = rows - 1) { cout << endl;}
             }   
             cout << endl;
         }
         */
+        //int TEST;
+        //cin >> TEST;
 
         myFile.close();
     }
